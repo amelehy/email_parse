5833abab784a1cf226a80e5a5613b2dcf55731fa
Alex Melehy <amelehy@gmail.com>
2016-10-29 17:19:31 -0400

    added tldextract lib and moved functionality to a class

--- main.py
+++ main.py
@@ -2,81 +2,116 @@
 
 import sys
 import requests
+import tldextract
 from bs4 import BeautifulSoup
 from urlparse import urlparse
+from urlparse import urljoin
 
-visited_urls = []
-found_email_addresses = []
-MAX_URLS_TO_VISIT = 50
+class EmailParser:
+
+    visited_urls = []
+    found_email_addresses = []
+    root_domain = ''
+    initial_url = ''
+    MAX_URLS_TO_VISIT = 50
+
+    def __init__(self, url):
+        parsed_url = urlparse(url)
+        self.initial_url = parsed_url.geturl() if parsed_url.scheme else 'http://' + parsed_url.geturl()
+        self.root_domain = tldextract.extract(self.initial_url).domain
+
+    def init_search(self):
+        self.init_link_discovery(self.initial_url)
+        final_emails = self.flatten_and_dedupe(self.found_email_addresses)
+        return final_emails
+
+    # initialize link discovery to recursively determine
+    # what links we should visit from the current page
+    def init_link_discovery(self, url):
+        response = self.init_request(url)
+        if response['success']:   
+            payload = response['payload']
+            raw_html = payload.text
+            soup = BeautifulSoup(raw_html, 'html.parser')
+            links_to_visit = self.fetch_all_links(soup)
+            for link in links_to_visit:
+                if link not in self.visited_urls and len(self.visited_urls) < self.MAX_URLS_TO_VISIT:
+                    print 'Visiting ' + link
+                    self.visited_urls.append(link)
+                    emails = self.init_link_discovery(link)
+                    if emails:
+                        self.found_email_addresses.append(emails)
+            return self.parse_emails(soup) 
+        else:
+            print 'Request to ' + url + ' failed because of: ' + str(response['payload'])
+
+    def init_request(self, url):
+        response = {'success': False, 'payload': ''}
+        try:
+            response['payload'] = requests.get(url)
+            response['success'] = True
+        except requests.exceptions.Timeout as e:
+            response['payload'] = e
+        except requests.exceptions.TooManyRedirects as e:
+            response['payload'] = e
+        except requests.exceptions.RequestException as e:
+            response['payload'] = e
+        return response
+
+    # fetch all relevant links in given page 
+    def fetch_all_links(self, html):
+        links = set()
+        for link in html.select('a'):
+            url = link.get('href') or ''
+            if self.root_domain in urlparse(url).netloc:
+                new_url = urljoin('http://' + self.root_domain, url)
+                links.add(new_url)
+        return links
+
+    # parse all emails given the html from a webpage
+    def parse_emails(self, html):
+        emails_found = []
+        for element in html.select('a[href^=mailto]'):
+            mailto_email = element['href']
+            index_of_colon = mailto_email.find(':') + 1
+            emails_found.append(mailto_email[index_of_colon:])
+        return emails_found
+
+    # given an array flatten and dedupe it
+    def flatten_and_dedupe(self, emails):
+        flattened = [val for sublist in emails for val in sublist]
+        deduped = set(flattened)
+        return deduped
+
 
 # initialize script
 def init():
-    # make sure that this script is not being imported as a module
+    # make sure that this script is not
+    # being imported as a module
     if __name__ == "__main__":
-        try:
-            root_domain = sys.argv[1]
-            init_link_discovery('http://' + root_domain)
-            final_emails = flatten_and_dedupe(found_email_addresses)
-            print_results(final_emails)
-        except IndexError:
-            print "Error: no argument passed for root domain"
-            sys.exit(1)
+        url = get_url_from_user()
+        emails = EmailParser(url).init_search()
+        print_results(emails)
+    else:
+        raise Exception('This script will be inactive if imported as a module.')
 
-# initialize link discovery to recursively determine
-# what links we should visit from the current page
-def init_link_discovery(url):
-    response = requests.get(url)
-    domain_name = urlparse(response.url).netloc
-    raw_html = response.text
-    soup = BeautifulSoup(raw_html, 'html.parser')
-    links_to_visit = fetch_all_links(soup, domain_name)
-    for link in links_to_visit:
-        if link not in visited_urls and len(visited_urls) < MAX_URLS_TO_VISIT:
-            print 'Visiting ' + link
-            visited_urls.append(link)
-            emails = init_link_discovery(link)
-            if emails:
-                found_email_addresses.append(emails)
-    return parse_emails(soup) 
-
-# fetch all relevant links in given page 
-def fetch_all_links(html, domain_name):
-    array = []
-    # get all urls that start with 'http' and 
-    # have the root domain name as a substring
-    for link in html.select('a[href^=http]'):
-        url = link.get('href')
-        if domain_name in urlparse(url).netloc:
-            array.append(url)
-    # get all urls that start with '/' 
-    for link in html.select('a[href^=\\/]'):
-        page = link.get('href')
-        # to account for situations like '//www.jana.com'
-        if '.com' in page:
-            array.append('http:' + page)
-        else:
-            array.append('http://' + domain_name + page)
-    return set(array)
-
-# parse all emails given the html from a webpage
-def parse_emails(html):
-    emails_found = []
-    for element in html.select('a[href^=mailto]'):
-        mailto_email = element['href']
-        index_of_colon = mailto_email.find(':') + 1
-        emails_found.append(mailto_email[index_of_colon:])
-    return emails_found
-
-# given an array flatten and dedupe it
-def flatten_and_dedupe(emails):
-    flattened = [val for sublist in emails for val in sublist]
-    deduped = set(flattened)
-    return deduped
+# fetch root domain from user input
+def get_url_from_user():
+    try:
+        return sys.argv[1]
+    except IndexError:
+        print "Error: no argument passed for root domain"
+        sys.exit(1)
+    except Exception as e:
+        print "Error: unable to parse the domain that was passed - " + str(e)
 
 # print out what we found
 def print_results(array):
     print '\nFound these email addresses:'
     for entry in array:
         print entry
+        break
+    else:
+        print 'None found'
 
 init()
