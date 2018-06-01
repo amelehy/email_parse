26755cf78cf8e67f75ef82a771a770f93220e9df
Alex Melehy <amelehy@gmail.com>
2018-06-01 17:25:01 -0400

    get max urls from user input

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def init():
     # being imported as a module
     if __name__ == "__main__":
         url = get_url_from_user()
-        max_urls_to_visit = 20
+        max_urls_to_visit = int(get_max_urls_to_visit_from_user())
         results = EmailParser(url, max_urls_to_visit).init_search()
         # Emails
         print '\nFound these email addresses:'
@@ -39,6 +39,15 @@ def get_url_from_user():
         sys.exit(1)
     except Exception as e:
         print "Error: unable to parse the domain that was passed - " + str(e)
+
+def get_max_urls_to_visit_from_user():
+    try:
+        return sys.argv[2]
+    except IndexError:
+        print "Error: no argument passed for max urls to visit"
+        sys.exit(1)
+    except Exception as e:
+        print "Error: unable to parse the max urls that was passed - " + str(e)
 
 # print out what we found
 def print_results(array):
